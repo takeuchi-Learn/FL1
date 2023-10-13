f65a04a9e069912c2cdab01a5575ad02ab046d95
Salvare000 <k020g1393@g.neec.ac.jp>
2023-10-13 15:14:49 +0900

    Playerクラスを追加

---
+++ Solution/App/Player/Player.h
@@ -0,0 +1,45 @@
+﻿/*****************************************************************//**
+ * \file   Player.h
+ * \brief  自機クラス
+ *********************************************************************/
+
+#pragma once
+
+#include <DirectXMath.h>
+#include <functional>
+#include <memory>
+
+class BaseGameObjectHavingHp;
+class Camera;
+class ObjModel;
+class Light;
+
+class Player
+{
+	std::unique_ptr<BaseGameObjectHavingHp> gameObj;
+	Light* light = nullptr;
+
+private:
+	/// @brief データをYAMLファイルから読み込む
+	/// @return エラーがあったかどうか（エラーでtrue）
+	bool loadYamlFile();
+
+public:
+
+	// コンストラクタ仮
+	Player(Camera* camera, ObjModel* model);
+
+	// 更新
+	void update();
+	// 描画
+	void draw();
+
+	// ジャンプ
+	void jump();
+
+	// 移動
+	void move();
+
+	inline void setLight(Light* light) { this->light = light; }
+};
+
