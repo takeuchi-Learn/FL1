cf86742cf0265846093e3b31e081b908b2f8d85b
Melon <ichikawajun12917@icloud.com>
2023-10-16 15:32:08 +0900

    カメラと注視点(プレイヤー指定予定)の距離設定

--- Solution/App/Scene/PlayScene.cpp
+++ Solution/App/Scene/PlayScene.cpp
@@ -111,6 +111,8 @@ PlayScene::PlayScene() :
 
 	camera->setEye(cameraPosDef);
 	camera->setTarget(objectPosDef);
+	// プレイヤーとカメラの距離を700に設定
+	camera->setEye2TargetLen(700.f);
 	pointLightPolarPos = cameraPosDef;
 
 	constexpr float lightAmbient = 0.f;
