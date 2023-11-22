d9464d35b73ce294cf06bbe0c85bde55a812df72
takeuchi-Learn <74523420+takeuchi-Learn@users.noreply.github.com>
2023-11-22 22:18:11 +0900

    ゲームオーバーにもBGMを追加

--- Solution/App/Scene/GameOverScene.cpp
+++ Solution/App/Scene/GameOverScene.cpp
@@ -1,18 +1,27 @@
 ﻿#include "GameOverScene.h"
-#include <2D/Sprite.h>
-#include <2D/DebugText.h>
 #include <Input/Input.h>
 #include <System/SceneManager.h>
+#include <2D/Sprite.h>
+#include <2D/SpriteBase.h>
+#include <Util/Util.h>
+#include <Sound/Sound.h>
+#include <algorithm>
 #include <PadImu.h>
 #include <JoyShockLibrary.h>
 
 #include "TitleScene.h"
 
-GameOverScene::GameOverScene() :
-	spriteBase(std::make_unique<SpriteBase>())
+using namespace DirectX;
+
+GameOverScene::GameOverScene()
 {
-	debugText = std::make_unique<DebugText>(spriteBase->loadTexture(L"Resources/debugfont.png"),
-											spriteBase.get());
+	spBase = std::make_unique<SpriteBase>();
+	sprite = std::make_unique<Sprite>(spBase->loadTexture(L"Resources/gameover.png"), spBase.get(), XMFLOAT2(0.f, 0.f));
+
+	bgm = Sound::ins()->loadWave("Resources/BGM/BGM.wav");
+	transitionSe = Sound::ins()->loadWave("Resources/SE/Shortbridge29-1.wav");
+
+	updateProc = std::bind(&GameOverScene::update_main, this);
 	thread = std::make_unique<std::jthread>([&] { nextScene = std::make_unique<TitleScene>(); });
 }
 
@@ -20,14 +29,28 @@ GameOverScene::~GameOverScene()
 {}
 
 void GameOverScene::start()
-{}
+{
+	Sound::playWave(bgm, XAUDIO2_LOOP_INFINITE, 0.4f);
+}
 
 void GameOverScene::update()
 {
-	debugText->Print("GameOver Scene\nHIT SPACE", 0.f, 0.f);
+	updateProc();
+}
 
-	if (thread->joinable() &&
-		checkInputOfStartTransition())
+void GameOverScene::update_main()
+{
+	if (checkInputOfStartTransition())
+	{
+		Sound::stopWave(bgm);
+		Sound::playWave(transitionSe, 0u, 0.2f);
+		updateProc = std::bind(&GameOverScene::update_end, this);
+	}
+}
+
+void GameOverScene::update_end()
+{
+	if (thread->joinable())
 	{
 		thread->join();
 		SceneManager::ins()->changeSceneFromInstance(nextScene);
@@ -59,6 +82,6 @@ bool GameOverScene::checkInputOfStartTransition()
 
 void GameOverScene::drawFrontSprite()
 {
-	spriteBase->drawStart(DX12Base::ins()->getCmdList());
-	debugText->DrawAll();
+	spBase->drawStart(DX12Base::ins()->getCmdList());
+	sprite->drawWithUpdate(DX12Base::ins(), spBase.get());
 }
--- Solution/App/Scene/GameOverScene.h
+++ Solution/App/Scene/GameOverScene.h
@@ -11,25 +11,27 @@
 
 class Sprite;
 class SpriteBase;
-class DebugText;
+class SoundData;
 
 /// @brief タイトル画面シーンのクラス
 class GameOverScene :
 	public GameScene
 {
-	// --------------------
-	// スプライト
-	// --------------------
-	std::unique_ptr<SpriteBase> spriteBase;
-
-	// --------------------
-	// デバッグテキスト
-	// --------------------
-	std::unique_ptr<DebugText> debugText;
-
 	std::unique_ptr<GameScene> nextScene = nullptr;
 	std::unique_ptr<std::jthread> thread{};
 
+	std::function<void()> updateProc{};
+
+	std::unique_ptr<Sprite> sprite;
+	std::unique_ptr<SpriteBase> spBase;
+
+	std::weak_ptr<SoundData> bgm;
+	std::weak_ptr<SoundData> transitionSe;
+
+	void update_main();
+	void update_end();
+
+	/// @return シーン遷移開始の入力があったかどうか
 	bool checkInputOfStartTransition();
 
 public:
