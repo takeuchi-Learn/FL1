5424dd3eff36667fbe872ac7e196797f67f0626a
Melolololon <75592470+Melolololon@users.noreply.github.com>
2023-12-15 16:44:32 +0900

    すり抜け床クラス作成。

---
+++ Solution/App/Object/SlipthroughFloor.cpp
@@ -0,0 +1,13 @@
+﻿#include "SlipthroughFloor.h"
+
+#include <GameCamera.h>
+#include <3D/Billboard/Billboard.h>
+
+SlipthroughFloor::SlipthroughFloor(GameCamera* camera, const DirectX::XMFLOAT2& pos, float scale)
+	:StageObject(camera, pos, scale, L"Resources/Map/Tex/goal.png")
+{
+	// 判定調整
+	
+
+
+}
---
+++ Solution/App/Object/SlipthroughFloor.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "StageObject.h"
+
+class GameCamera;
+class Billboard;
+
+// 今の状態だとマップチップで指定できない為、どうにかする
+// 地形判定の配列とは別の配列を用意し、すり抜け床だけ隔離する
+// で、GameMapから取得できるようにし、typeid(GameMap)で地形か確認していた部分を地形とすり抜け床で分ける
+// ゴールもこれでいいかもしれない
+
+// すり抜け床 戦場のあれ
+class SlipthroughFloor :
+    public StageObject
+{
+private:
+
+public:
+	SlipthroughFloor(GameCamera* camera, const DirectX::XMFLOAT2& pos, float scale);
+};
+
