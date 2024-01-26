0e81644938d35b75bf69b3f0965130d3eacaa4ec
takeuchi-Learn <74523420+takeuchi-Learn@users.noreply.github.com>
2024-01-26 14:34:39 +0900

    チュートリアルの画像を変更

--- Solution/App/TutorialTexture.cpp
+++ Solution/App/TutorialTexture.cpp
@@ -6,11 +6,18 @@ using namespace DirectX;
 
 namespace
 {
-	constexpr const wchar_t texPath[] = L"Resources/Map/Tex/tutorial.png";
+	constexpr const wchar_t* texPath[] =
+	{
+		L"Resources/tutorial/tutorial.png",
+		L"Resources/tutorial/tutorial2.png"
+	};
+
+	constexpr uint16_t TUTORIAL_MOVE = 0ui16;
+	constexpr uint16_t TUTORIAL_JUMP = 1ui16;
 };
 
 TutorialTexture::TutorialTexture(GameCamera* camera, const uint16_t stageNum)
-	: gameObj(std::make_unique<Billboard>(texPath, camera))
+	: gameObj(std::make_unique<Billboard>(texPath[std::min(stageNum, 1ui16)], camera))
 	, camera(camera)
 	, STAGE_NUM(stageNum)
 {
@@ -33,9 +40,6 @@ void TutorialTexture::draw()
 
 void TutorialTexture::createState()
 {
-	constexpr decltype(STAGE_NUM) TUTORIAL_MOVE = 0ui16;
-	constexpr decltype(STAGE_NUM) TUTORIAL_JUMP = 1ui16;
-
 	switch (STAGE_NUM)
 	{
 	case TUTORIAL_MOVE:
